1af62e4f8688f1cf0005b62d54b6cc64794d5cdc
Tim Andrews <tim.andrews@imagineifinc.com>
2015-11-17 12:48:28 -0500

    q1.1 done!

--- q1.1.py
+++ q1.1.py
@@ -9,39 +9,41 @@ class Meeting(object):
      def __init__(self, name, attendees):
          self.name = name
          inputList = list(attendees)
+
+         #DeDup original attendee list when Meeting Obj is first initiated
          attendee_names = set()
          noDuplicateAttendeeList = []
          for obj in inputList:
             if obj.name not in attendee_names:
                 noDuplicateAttendeeList.append(obj)
                 attendee_names.add(obj.name)
+            else:
+                print "dupe"
          self.attendees = noDuplicateAttendeeList
 
      def add_attendee(self, person):
-         
-         self.attendees.append(person)
+         print person.name
+         attendee_names = set()
+
+         #Check to make sure new attendee is not a duplicate before appending to obj
+         #Build list of existing attendee names
+         for obj in self.attendees:
+             attendee_names.add(obj.name)
+
+         #Add new name if not duplicate
+         if person.name not in attendee_names:
+            self.attendees.append(person)
+         else:
+            print "dupe"
 
      def calculate_fees(self):
          return sum(a.fee for a in self.attendees)
      def how_many_lunches_needed(self):
          return len(self.attendees)
 
-class DeDupe(object):
-    def __init__(self, attendees):
-        self.attendees = attendees
-        attendee_names = set()
-        noDuplicateAttendeeList = []
-        for obj in self.attendees:
-            if obj.name not in attendee_names:
-                noDuplicateAttendeeList.append(obj)
-                attendee_names.add(obj.name)
-
-        for x in noDuplicateAttendeeList:
-            print "x   ", x.name
-
-        return noDuplicateAttendeeList
 
 def main():
+     #Original attendee list contains one duplicate
      attendees = [
                    Attendee("John Allston", 50),
                    Attendee("Margaret Bristol", 50),
@@ -54,18 +56,12 @@ def main():
 
      our_meeting = Meeting("Major Conference", attendees)
 
-     #our_meeting.add_attendee(Attendee("John Sample", 30))
-     #our_meeting.add_attendee(Attendee("Pat Smith", 50))
-
-     for attendee in our_meeting.attendees:
-        print "c:    ", attendee.name, ", ", attendee.fee
-
-     print
+     #Add two additional attendees one of which is a duplicate
+     our_meeting.add_attendee(Attendee("John Sample", 30))
+     our_meeting.add_attendee(Attendee("Pat Smith", 50))
 
      print "Total fees are: ", our_meeting.calculate_fees()
      print "lunches needed: ", our_meeting.how_many_lunches_needed()
-
-
 
 
 if __name__=="__main__":
